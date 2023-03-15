af568646c652be4826ae2792bf8d239cf006073f
Mate Mong <mongmate811@gmail.com>
2023-03-15 23:25:04 +0100

    basic socket connection

---
+++ src/pages/api/socket.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Server } from "Socket.IO";
+
+type Data = {
+  name: string;
+};
+
+export default function SocketHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+  }
+  res.end();
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import io from "socket.io-client";
+import { useEffect } from "react";
+let socket;
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  useEffect(() => {
+    async function socketInitializer() {
+      await fetch("/api/socket");
+      socket = io();
+
+      socket.on("connect", () => {
+        console.log("connected");
+      });
+    }
+    socketInitializer();
+  }, []);
+
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
